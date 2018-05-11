b087503b7f89b3ed16146e6a6ee2963f754334d9
Rodrigo Martins <xkirtle26@gmail.com>
2018-05-11 19:16:35 +0100

    Added the first sword "Elucidator"

---
+++ Weapons/Elucidator.cs
@@ -0,0 +1,41 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Kirtle.Items.Weapons
+{
+    public class Elucidator : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Elucidator");
+            Tooltip.SetDefault("Dropped from a demonic monster at the 50th floor.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 50;
+            item.melee = true;
+            item.width = 45;
+            item.height = 41;
+            item.useTime = 19;
+            item.useAnimation = 19;
+            item.useStyle = 1;
+            item.knockBack = 9;
+            item.value = 10000;
+            item.rare = 2;
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = true;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.DirtBlock, 10);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
